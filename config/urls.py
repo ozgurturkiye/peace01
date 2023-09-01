031878e9db870d745b44fb5003b683660d2f311d
Özgür <ozgurturkiye@gmail.com>
2023-09-01 22:31:02 +0300

    feat: add API TOKEN jd-rest-auth

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/", include("apis.urls")),
 ]
