7a323671af5311afaa1f905148024c07a8a0e659
Özgür <ozgurturkiye@gmail.com>
2023-09-01 15:41:35 +0300

    feat: add simple model English test

--- words/tests.py
+++ words/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from words.models import English
+
+
+class EnglishTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.english = English.objects.create(name="abandon", word_type="V")
+
+    def test_english_content(self):
+        self.assertEqual(self.english.name, "abandon", "“Why, Mr. Anderson?, Why, why?")
+        self.assertEqual(self.english.word_type, "V")
